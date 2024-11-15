f1a8989607be4c46fdbf2a68ed4d40c465ba5543
Marcap00 <cappellatomarco@yahoo.com>
2024-11-15 23:42:12 +0100

    change loader

--- src/components/general/BaseLoader.vue
+++ src/components/general/BaseLoader.vue
@@ -14,34 +14,23 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+/* HTML: <div class="loader"></div> */
 .loader {
     width: 50px;
     aspect-ratio: 1;
-    display: grid;
-    color: #000000;
+    border-radius: 50%;
     background:
-        conic-gradient(from 90deg at 3px 3px, #0000 90deg, currentColor 0) -3px -3px/calc(50% + 1.5px) calc(50% + 1.5px);
-    animation: l28 2s infinite;
+        radial-gradient(farthest-side, #ffffff 94%, #0000) top/8px 8px no-repeat,
+        conic-gradient(#0000 30%, #ffffff);
+    -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 8px), #000 0);
+    animation: l13 1s infinite linear;
     /* Centering */
     position: absolute;
     top: 50%;
     left: 50%;
 }
 
-.loader::before,
-.loader::after {
-    content: "";
-    grid-area: 1/1;
-    background: repeating-conic-gradient(#0000 0 35deg, currentColor 0 90deg);
-    -webkit-mask: radial-gradient(farthest-side, #0000 calc(100% - 3px), #000 0);
-    border-radius: 50%;
-}
-
-.loader::after {
-    margin: 20%;
-}
-
-@keyframes l28 {
+@keyframes l13 {
     100% {
         transform: rotate(1turn)
     }
