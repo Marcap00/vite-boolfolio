24fea96c4efc09a6d35777e740f775b6c39faf4a
Marcap00 <cappellatomarco@yahoo.com>
2024-11-18 15:45:32 +0100

    add route for ProjectsDetails component and create component

--- src/js/router.js
+++ src/js/router.js
@@ -3,10 +3,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 import AppHome from '@/pages/AppHome.vue'
 import AppProjects from '@/pages/AppProjects.vue'
 import AppTechnology from '@/pages/AppTechnology.vue'
+import ProjectsShow from '@/components/projects/ProjectsDetails.vue'
 
 const routes = [
     { path: '/', component: AppHome, name: 'home' },
     { path: '/projects', component: AppProjects, name: 'projects' },
+    { path: '/projects/:id', component: ProjectsShow, name: 'projects.show' },
     { path: '/technologies', component: AppTechnology, name: 'technologies' }
 ]
 
