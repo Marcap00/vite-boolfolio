d9ed5bbd7f9fb67e72a8a8fecb11320402b66a59
Marcap00 <cappellatomarco@yahoo.com>
2024-11-18 16:10:57 +0100

    created component for projects details

--- src/js/router.js
+++ src/js/router.js
@@ -3,12 +3,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 import AppHome from '@/pages/AppHome.vue'
 import AppProjects from '@/pages/AppProjects.vue'
 import AppTechnology from '@/pages/AppTechnology.vue'
-import ProjectsShow from '@/components/projects/ProjectsDetails.vue'
+import AppProjectsDetails from '@/pages/AppProjectsDetails.vue'
 
 const routes = [
     { path: '/', component: AppHome, name: 'home' },
     { path: '/projects', component: AppProjects, name: 'projects' },
-    { path: '/projects/:id', component: ProjectsShow, name: 'projects.show' },
+    { path: '/projects/:id', component: AppProjectsDetails, name: 'projects.show' },
     { path: '/technologies', component: AppTechnology, name: 'technologies' }
 ]
 
---
+++ src/pages/AppProjectsDetails.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppProjectsDetails",
+    components: {},
+    props: {},
+    data() {
+        return {
+
+        };
+    },
+    methods: {},
+    computed: {},
+};
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
